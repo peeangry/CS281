081ecb830eb4b1b0c99469292e420f54309fcbf3
Tasit Sappooree <jetzjamez@hotmail.com>
2017-11-09 22:31:17 +0700

    Update Test by JorJet

--- test/TestExport.java
+++ test/TestExport.java
@@ -12,14 +12,53 @@ class TestExport {
 
 
 	@Test
-	public void testcanExportFile()
-	{
-			assertTrue(FileConection.saveFileExport());//
+	public void testcanExportFile() {
+		assertTrue(FileConection.saveFileExport());
+	
 	}
+	
 	@Test
-	public void testExporttocancel()
-	{
-			assertFalse(FileConection.saveFileExport());
+	public void testExporttocancel() {
+		assertFalse("Error",FileConection.saveFileExport());
+	}
+	
+	@Test
+	public void testDecimal() {
+		
+		
+		//1.insert score number with decimal
+			
+		//2.save
+		
+	}
+	
+	@Test
+	public void testBoxInput() {
+		//1.don't input any number in box
+		
+		//2.save
+		
+	}
+	
+	@Test
+	public void testNegativeNumber() {
+		//1.try input a negative number in box
+		
+		
+		//2.save
+		
+	}
+	
+	@Test
+	public void testEqualScores() {
+		
+		
+	}
+	
+	@Test
+	public void testTotalScores() {
+		
+		
 	}
 
 }
